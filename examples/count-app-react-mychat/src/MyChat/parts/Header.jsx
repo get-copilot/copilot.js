bc2c7f953c9283e368503fc3fffe5bec6811bcb2
dream-fx <pc@copilotjs.com>
2024-10-08 09:45:47 -0700

    upgrade to latest SDK version

--- examples/count-app-react-mychat/src/MyChat/parts/Header.jsx
+++ examples/count-app-react-mychat/src/MyChat/parts/Header.jsx
@@ -3,8 +3,8 @@ import { XMarkIcon, DocumentIcon } from '@heroicons/react/24/outline'
 import { useStore } from '../store'
 
 export default function Header() {
-  const { setIsOpen } = useStore()
-  const { status, reset } = useCopilot()
+  const { setIsOpen, isResetting, setIsResetting } = useStore()
+  const { createAndSelectThread } = useCopilot()
 
   return (
     <div className="flex-none flex flex-row justify-between border-b">
@@ -23,8 +23,12 @@ export default function Header() {
 
       {/* Reset button */}
       <button
-        onClick={() => reset()}
-        disabled={!(status === 'idle' || status === 'working' || status === 'cancelling')}
+        onClick={async () => {
+          setIsResetting(true)
+          await createAndSelectThread()
+          setIsResetting(false)
+        }}
+        disabled={isResetting}
         className="flex-none hover:bg-gray-100 disabled:opacity-10"
       >
         <DocumentIcon className="w-5 h-5 mx-3 my-3.5" />
--- examples/count-app-react-mychat/src/MyChat/store.js
+++ examples/count-app-react-mychat/src/MyChat/store.js
@@ -3,15 +3,13 @@ import { create } from 'zustand'
 export const useStore = create((set, get) => ({
   // The visibility state of the copilot.
   isOpen: true,
+  setIsOpen: (isOpen) => set({ isOpen }),
 
-  setIsOpen: (isOpen) => {
-    set({ isOpen: isOpen })
-  },
+  // Is the conversation being reset.
+  isResetting: false,
+  setIsResetting: (isResetting) => set({ isResetting }),
 
   // Draft of the user prompt.
   draft: '',
-
-  setDraft: (draft) => {
-    set({ draft: draft })
-  },
+  setDraft: (draft) => set({ draft }),
 }))
